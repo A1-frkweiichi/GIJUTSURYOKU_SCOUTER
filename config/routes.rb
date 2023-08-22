7e2251e9cd7e8ba91e8c1231db02805138b89961
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-22 23:03:37 +0900

    login post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "home#top"
-  get 'auth/:provider/callback', to: 'sessions#create'
+  post 'auth/:provider/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
 end
